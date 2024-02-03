0b2591ed0eeaf88a1ad227063253bc7af6cf033d
TheLazyIndianTechie <86476092+TheLazyIndianTechie@users.noreply.github.com>
2024-02-03 19:06:36 +0530

    Created explosion prefabs and updated grenade script to handle explosions

--- Assets/Scripts/Grenade.cs
+++ Assets/Scripts/Grenade.cs
@@ -1,10 +1,13 @@
-using System;
 using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
     [SerializeField]
     private float _force = 10f;
+    
+    [SerializeField]
+    private GameObject _explosionPrefab;
+    
     // Start is called before the first frame update
     
     void Start()
@@ -13,5 +16,27 @@ public class Grenade : MonoBehaviour
         GetComponent<Rigidbody2D>().AddForce(transform.forward * _force, ForceMode2D.Impulse);
     }
 
-    private void OnEnable() => Destroy(gameObject, 5f);
+    private void OnEnable()
+    {
+        // Spawn explosion after object is enabled
+        Destroy(gameObject, 5f);
+    }
+    
+    // Create a coroutine to spawn the explosion prefab after the current game object is destroyed
+    private void OnDestroy()
+    {
+        if (_explosionPrefab == null) return;
+        var explosionPrefab = Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(explosionPrefab, 0.1f);
+            
+        //TODO: Create a scaling lerp animation
+            
+        // // Smoothly lerp using lerp function to explosion prefab scale from 0 to 1 over 0.5 seconds
+        // explosionPrefab.transform.localScale = Vector3.zero;
+        // LeanTween.scale(explosionPrefab, Vector3.one, 0.5f);
+
+
+
+
+    }
 }
